7aec54b26ff1523f81f06a74025e1f8706060c13
igorcretu <64871246+igorcretu@users.noreply.github.com>
2024-05-09 13:28:08 +0200

    input box update

--- frontend/src/components/InputBox/InputBox.tsx
+++ frontend/src/components/InputBox/InputBox.tsx
@@ -5,39 +5,40 @@ import "./InputBox.css";
 import { IconType } from "react-icons";
 
 function InputBox({
-  label,
-  type,
-  InputIcon,
-  handleClick,
+	label,
+	type,
+	InputIcon,
+	handleClick,
 }: {
-  label: string;
-  type: string;
-  InputIcon: IconType;
-  handleClick:any
+	label: string;
+	type: string;
+	InputIcon?: IconType;
+	handleClick: any;
 }) {
-  const [value, setValue] = useState("");
+	const [value, setValue] = useState("");
 
-  return (
-    <div className="form_input">
-      <div className="form_input_box">
-        <label htmlFor="">{label}</label>
-        <input
-          type={type}
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          onSubmit={handleClick(value)}
-        ></input>
-      </div>
-      <InputIcon  style={
-        {
-        
-          width: "40px",
-          height: "40px",
-          fill: "var(--color-secondary)"
-        }
-      } />
-    </div>
-  );
+	return (
+		<div className="form_input">
+			<div className="form_input_box">
+				<label htmlFor="">{label}</label>
+				<input
+					type={type}
+					value={value}
+					onChange={(e) => setValue(e.target.value)}
+					onSubmit={handleClick(value)}
+				></input>
+			</div>
+			{InputIcon && (
+				<InputIcon
+					style={{
+						width: "40px",
+						height: "40px",
+						fill: "var(--color-secondary)",
+					}}
+				/>
+			)}
+		</div>
+	);
 }
 
 export default InputBox;
