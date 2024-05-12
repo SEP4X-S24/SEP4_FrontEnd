0870394718674f7dd5de6b65cbfef8c93de9b6a5
DanTurcanIuliu <turcan.dan.ro@gmail.com>
2024-05-12 02:57:33 +0200

    Add Header and Footer

--- frontend/src/pages/Recomendation/RecomendationPage.tsx
+++ frontend/src/pages/Recomendation/RecomendationPage.tsx
@@ -3,6 +3,8 @@ import { useAuth } from "../../services/auth/AuthContext";
 import WeatherOutfitBanner from "./components/WeatherOutfitBanner/WeatherOutfitBanner";
 import WeatherInfo from "./components/WeatherInfo/WeatherInfo";
 import ItemCollection from "./components/ItemCollection/ItemCollection";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 function RecomendationPage() {
   const { isAuthenticated: authenticated } = useAuth();
@@ -12,9 +14,11 @@ function RecomendationPage() {
 
   return (
     <div className="app">
+      <Header />
       <WeatherOutfitBanner />
       <WeatherInfo />
       <ItemCollection />
+      <Footer />
       {/* Other components will follow */}
     </div>
   );
