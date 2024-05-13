b75af6b620b831933b837bffbece954081c1d078
igorcretu <64871246+igorcretu@users.noreply.github.com>
2024-05-13 12:46:36 +0200

    code cleaning

--- frontend/src/pages/AccountSettings/NotificationSetting.tsx
+++ frontend/src/pages/AccountSettings/NotificationSetting.tsx
@@ -1,72 +1,10 @@
-import React, { useState } from "react";
 import InputBox from "../../components/InputBox/InputBox";
 import "./NotificationSettings.css";
 
 function NotificationSetting() {
-	// const [rainNoficiation, setRainNotification] = useState(true);
-	// const [windNotification, setWindNotification] = useState(true);
-	// const [minusDegreeNotification, setMinusDegreeNotification] = useState(true);
-	// return (
-	//   <div className="container">
-	//     <div className="row justify-content-center">
-	//       <div className="col-md-6">
-	//         <div className="p-3 bg-white rounded shadow-sm">
-	//           <h3>Notification Settings</h3>
-	//           <form>
-	//             <div className="mb-3 form-control d-flex justify-content-between">
-	//               <label className="form-check-label" htmlFor="notifications">
-	//                 Notify when is raining
-	//               </label>
-	//               <div className="form-check">
-	//                 <input
-	//                   type="checkbox"
-	//                   className="form-check-input"
-	//                   id="rain"
-	//                   checked={rainNoficiation}
-	//                   onChange={() => setRainNotification(!rainNoficiation)}
-	//                 />
-	//               </div>
-	//             </div>
-
-	//             <div className="mb-3 form-control d-flex justify-content-between">
-	//               <label className="form-check-label" htmlFor="recommendations">
-	//                 Notify when strong wind is blowing
-	//               </label>
-	//               <div className="form-check">
-	//                 <input
-	//                   type="checkbox"
-	//                   className="form-check-input"
-	//                   id="wind"
-	//                   checked={windNotification}
-	//                   onChange={() => setWindNotification(!windNotification)}
-	//                 />
-	//               </div>
-	//             </div>
-
-	//             <div className="mb-3 form-control d-flex justify-content-between">
-	//               <label className="form-check-label" htmlFor="recommendations">
-	//                 Notify when is minus degree Celsius
-	//               </label>
-	//               <div className="form-check">
-	//                 <input
-	//                   type="checkbox"
-	//                   className="form-check-input"
-	//                   id="minuDegree"
-	//                   checked={windNotification}
-	//                   onChange={() => setWindNotification(!windNotification)}
-	//                 />
-	//               </div>
-	//             </div>
-	//           </form>
-	//         </div>
-	//       </div>
-	//     </div>
-	//   </div>
-	// );
-
 	let rain = false;
-  let wind = false;
-  let temperature = false;
+	let wind = false;
+	let temperature = false;
 
 	function handleNotificationRain(data: boolean) {
 		rain = !data;
@@ -75,7 +13,7 @@ function NotificationSetting() {
 		wind = !data;
 	}
 	function handleNotificationTemperature(data: boolean) {
-    temperature = !data;
+		temperature = !data;
 	}
 	function sendData() {
 		// change on implementation of logic
