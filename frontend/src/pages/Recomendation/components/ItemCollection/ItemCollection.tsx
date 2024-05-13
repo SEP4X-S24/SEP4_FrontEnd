c8661861c35e7febf03b5cc12998b9164d483464
DanTurcanIuliu <turcan.dan.ro@gmail.com>
2024-05-13 02:15:38 +0200

    ItemCollection

--- frontend/src/pages/Recomendation/components/ItemCollection/ItemCollection.tsx
+++ frontend/src/pages/Recomendation/components/ItemCollection/ItemCollection.tsx
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import "./ItemCollection.css";
+import rainPants from "./Items/RainPants.webp";
+import raincoat from "./Items/Raincoat.webp";
+import rainhat from "./Items/RainHat.webp";
+import rubberShoes from "./Items/RubberShoes.webp";
+import umbrella from "./Items/Umbrella.webp";
+import waterproofBoots from "./Items/WaterproofBoots.webp";
+import waterproofJacket from "./Items/WaterproofJacket.webp";
 
 const items = [
-  { id: 1, name: "Rain Pants", imageUrl: "./Items/Umbrella.webp" },
-  { id: 2, name: "Raincoat", imageUrl: "/items/Raincoat.webp" },
-  { id: 3, name: "Rainhat", imageUrl: "/items/Rainhat.webp" },
-  { id: 4, name: "Rubber Shoes", imageUrl: "/items/RubberShoes.webp" },
-  { id: 5, name: "Umbrella", imageUrl: "/items/Umbrella.webp" },
-  { id: 6, name: "Waterproof Boots", imageUrl: "/items/WaterproofBoots.webp" },
-  {
-    id: 7,
-    name: "Waterproof Jacket",
-    imageUrl: "/items/WaterproofJacket.webp",
-  },
+  { id: 1, name: "Rain Pants", imageUrl: rainPants },
+  { id: 2, name: "Raincoat", imageUrl: raincoat },
+  { id: 3, name: "Rainhat", imageUrl: rainhat },
+  { id: 4, name: "Rubber Shoes", imageUrl: rubberShoes },
+  { id: 5, name: "Umbrella", imageUrl: umbrella },
+  { id: 6, name: "Waterproof Boots", imageUrl: waterproofBoots },
+  { id: 7, name: "Waterproof Jacket", imageUrl: waterproofJacket },
 ];
 
 const ItemCollection: React.FC = () => {
