cd7997e699163aff2450908b2abea964fbdc7844
igorcretu <64871246+igorcretu@users.noreply.github.com>
2024-05-23 16:18:36 +0200

    Update Recommendation weather data

--- frontend/src/pages/Recomendation/components/WeatherInfo/WeatherInfo.tsx
+++ frontend/src/pages/Recomendation/components/WeatherInfo/WeatherInfo.tsx
@@ -1,64 +1,70 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./WeatherInfo.css";
-import * as Icon from "react-icons/wi";
 import * as Icons from "react-icons/wi";
-import DummyWeatherService from "../../../../services/impl/DummyWeatherService";
+import WeatherHttpService from "../../../../services/impl/WeatherHttpService";
+import CurrentWeather from "../../../../models/CurrentWeather";
+import weatherFetcher from "../../../../services/impl/WeatherFetcher";
 
 const WeatherInfo: React.FC = () => {
-  const [weather, setWeather] = useState({
-    temperature: 0,
-    weatherState: "",
-    humidity: 0,
-    windSpeed: 0,
-  });
+  const [currentWeather, setCurrentWeather] = useState<CurrentWeather | null>(
+		null
+	);
+    const isWeatherLoadedRef = useRef(false);
+
 
   useEffect(() => {
-    const weatherService = new DummyWeatherService();
-    weatherService.fetchWeatherHourlyForecast().then((hourlyForecast) => {
-      if (hourlyForecast && hourlyForecast.length > 0) {
-        const currentWeather = hourlyForecast[0]; // Assuming the first item is the current weather
-        setWeather({
-          temperature: currentWeather.temperature,
-          weatherState: currentWeather.weatherState,
-          humidity: 56, // Assuming a constant for demonstration
-          windSpeed: 56, // Assuming a constant for demonstration
-        });
-      }
-    });
+    // const weatherService = new DummyWeatherService();
+    const service = new WeatherHttpService();
+
+		const fetchWeatherData = async () => {
+			const cachedWeather = localStorage.getItem("current_weather");
+			if (cachedWeather) {
+				setCurrentWeather(JSON.parse(cachedWeather));
+			} else {
+				if (!isWeatherLoadedRef.current) {
+					const fetchedWeather = await weatherFetcher.initCurrentWeatherFetch();
+					setCurrentWeather(fetchedWeather);
+				}
+			}
+		};
+
+		fetchWeatherData();
+        isWeatherLoadedRef.current = true;
+
   }, []);
 
   // Dynamic icon retrieval with type assertion
-  const WeatherIcon = weather.weatherState
-    ? (Icons as any)[`Wi${weather.weatherState}`]
-    : Icons.WiDaySunny;
+  const WeatherIcon = currentWeather?.weatherState
+		? (Icons as any)[`Wi${currentWeather?.weatherState}`]
+		: Icons.WiDaySunny;
 
   return (
-    <div className="weather-info-container">
-      <div className="weather-detail">
-        {WeatherIcon ? (
-          <WeatherIcon size={50} />
-        ) : (
-          <Icons.WiDaySunny size={50} />
-        )}
-        <div className="weather-detail">
-          <span>{weather.weatherState || "Clear"}</span>{" "}
-          {/* Default text if state is undefined */}
-        </div>
-      </div>
-      <div className="weather-detail">
-        <span className="weather-value">{weather.temperature}°</span>
-        <span className="weather-label">Temperature</span>
-      </div>
-      <div className="weather-detail">
-        <span className="weather-value">{weather.humidity}%</span>
-        <span className="weather-label">Humidity</span>
-      </div>
-      <div className="weather-detail">
-        <span className="weather-value">{weather.windSpeed}m/s</span>
-        <span className="weather-label">Wind</span>
-      </div>
-    </div>
-  );
+		<div className="weather-info-container">
+			<div className="weather-detail">
+				{WeatherIcon ? (
+					<WeatherIcon size={50} />
+				) : (
+					<Icons.WiDaySunny size={50} />
+				)}
+				<div className="weather-detail">
+					<span>{currentWeather?.weatherState || "Clear"}</span>{" "}
+					{/* Default text if state is undefined */}
+				</div>
+			</div>
+			<div className="weather-detail">
+				<span className="weather-value">{currentWeather?.temperature}°</span>
+				<span className="weather-label">Temperature</span>
+			</div>
+			<div className="weather-detail">
+				<span className="weather-value">{currentWeather?.humidity}%</span>
+				<span className="weather-label">Humidity</span>
+			</div>
+			<div className="weather-detail">
+				<span className="weather-value">{currentWeather?.light}</span>
+				<span className="weather-label">Light</span>
+			</div>
+		</div>
+	);
 };
 
 export default WeatherInfo;
