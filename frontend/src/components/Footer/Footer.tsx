730bb5a6198d2c22160e2ca5067dd2e8fa217981
Sevastian Bahynskyi <baginski.play@gmail.com>
2024-05-28 18:28:52 +0200

    UPDATE: footer about section

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -4,14 +4,14 @@ import * as SMIcon from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
 
 function Footer() {
-  const aboutDetails = "Lorem ipsum In convallis justo at eleifend sagittis.";
+  const aboutDetails = "Starting from the fact that weather is dynamic and can vary significantly even in the one location, it is often essential to have a clear picture of the meteorological conditions at a fixed point at a moment in time, not just some average values. Our system provides a solution for a technical and digital tool that would allow the measurement of meteorological conditions in a specific location and to present the results to users in live mode. Also to give suggestions about clothes in real time considering the weather conditions.";
   const currentYear = new Date().getFullYear();
   const socialMediaIconSize = 35;
 
   return (
     <footer className="footer">
       <div className="container main-section d-flex flex-wrap">
-        <div className="footer-section about-section col-md-6">
+        <div className="footer-section about-section col-md-5">
           <h3>About</h3>
           <p className="m-0">{aboutDetails}</p>
         </div>
